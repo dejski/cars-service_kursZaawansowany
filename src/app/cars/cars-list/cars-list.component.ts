f8d9ca4ae4fa511204353ddde5837bfc8b913a7f
dejski <dejski@wp.pl>
2018-08-15 10:53:25 +0200

    rxjs - komunikowanie miedzy komponentami

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit,AfterViewInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import {Car} from "../models/car";
-import {TotalCostComponent} from "../total-cost/total-cost.component";
-import {CarsService} from "../cars.service";
-import {Router} from "@angular/router";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewEncapsulation,
+  ViewChild,
+} from '@angular/core'
+import { Car } from '../models/car'
+import { TotalCostComponent } from '../total-cost/total-cost.component'
+import { CarsService } from '../cars.service'
+import { Router } from '@angular/router'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { CostSharedService } from '../cost-shared.service'
 
 @Component({
   selector: 'cs-cars-list',
   templateUrl: './cars-list.component.html',
   styleUrls: ['./cars-list.component.less'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class CarsListComponent implements OnInit {
-  @ViewChild("totalCostRef") totalCostRef : TotalCostComponent;
-  totalCost : number;
-  grossCost : number;
-  cars : Car[] = [];
-  carForm : FormGroup;
+  @ViewChild('totalCostRef')
+  totalCostRef: TotalCostComponent
+  totalCost: number
+  grossCost: number
+  cars: Car[] = []
+  carForm: FormGroup
 
-  constructor(private carsService : CarsService,
-              private formBuilder : FormBuilder,
-              private router : Router) {}
+  constructor(
+    private carsService: CarsService,
+    private formBuilder: FormBuilder,
+    private costSharedService: CostSharedService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.loadCars();
-    this.carForm = this.buildCarForm();
+    this.loadCars()
+    this.carForm = this.buildCarForm()
   }
 
   buildCarForm() {
     return this.formBuilder.group({
       model: ['', Validators.required],
       type: '',
-      plate: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(7)]],
+      plate: [
+        '',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(7)],
+      ],
       deliveryDate: '',
       deadline: '',
       color: '',
@@ -40,49 +54,48 @@ export class CarsListComponent implements OnInit {
       clientSurname: '',
       cost: '',
       isFullyDamaged: '',
-      year: ''
-    });
+      year: '',
+    })
   }
 
-  loadCars() : void {
-    this.carsService.getCars().subscribe((cars) => {
-      this.cars = cars;
-      this.countTotalCost();
-    });
+  loadCars(): void {
+    this.carsService.getCars().subscribe(cars => {
+      this.cars = cars
+      this.countTotalCost()
+      this.costSharedService.shareCost(this.totalCost)
+    })
   }
 
   addCar() {
     this.carsService.addCar(this.carForm.value).subscribe(() => {
-      this.loadCars();
-    });
+      this.loadCars()
+    })
   }
 
-  goToCarDetails(car : Car) {
-    this.router.navigate(['/cars', car.id]);
+  goToCarDetails(car: Car) {
+    this.router.navigate(['/cars', car.id])
   }
 
-  removeCar(car : Car, event) {
-    event.stopPropagation();
+  removeCar(car: Car, event) {
+    event.stopPropagation()
     this.carsService.removeCar(car.id).subscribe(() => {
-      this.loadCars();
-    });
+      this.loadCars()
+    })
   }
 
-  showGross() : void {
-    this.totalCostRef.showGross();
+  showGross(): void {
+    this.totalCostRef.showGross()
   }
 
-  countTotalCost() : void {
+  countTotalCost(): void {
     if (this.cars.length === 0) {
-      return;
+      return
     }
 
-    this.totalCost = this.cars
-      .map((car) => car.cost)
-      .reduce((prev, next) => prev + next);
+    this.totalCost = this.cars.map(car => car.cost).reduce((prev, next) => prev + next)
   }
 
-  onShownGross(grossCost : number) : void {
-    this.grossCost = grossCost;
+  onShownGross(grossCost: number): void {
+    this.grossCost = grossCost
   }
 }
--- src/app/cars/total-cost/income-tax/income-tax.component.ts
+++ src/app/cars/total-cost/income-tax/income-tax.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { CostSharedService } from '../../cost-shared.service'
+import { Subscription } from 'rxjs/Subscription'
 
 @Component({
   selector: 'cs-income-tax',
   templateUrl: './income-tax.component.html',
-  styleUrls: ['./income-tax.component.less']
 })
-export class IncomeTaxComponent implements OnInit {
+export class IncomeTaxComponent implements OnInit, OnDestroy {
+  private incomeTax: number = 18
+  income: number
+  costSubscription: Subscription
 
-  constructor() { }
+  constructor(private costSharedService: CostSharedService) {}
 
   ngOnInit() {
+    this.costSubscription = this.costSharedService.totalCostSource$.subscribe(cost => {
+      this.income = (cost * this.incomeTax) / 100
+    })
   }
 
+  ngOnDestroy() {
+    if (this.costSubscription) {
+      this.costSubscription.unsubscribe()
+    }
+  }
 }
